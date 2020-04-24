8cfc022cb1fa33410379337403539aac594b76ad
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-04-24 12:53:25 +0300

    Add files via upload

---
+++ Adaptive/script.js
@@ -0,0 +1,12 @@
+function f1() {
+	a = document.getElementById('name').value;
+	result = document.getElementById('mail').innerHTML;
+	document.getElementById('name').value;
+
+	result.innerHTML;
+
+
+	document.getElementById('mail').innerHTML = a;
+
+	alert(a);
+}
