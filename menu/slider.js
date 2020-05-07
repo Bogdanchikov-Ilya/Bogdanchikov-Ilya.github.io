cc96c4815b36de35cc334ea327acd31744743972
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-05-07 23:09:35 +0300

    Add files via upload

---
+++ menu/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHight:true,
+		slidesToShow:1,
+		autoplay:true,
+		autoplaySpeed:2200,
+		centerMode:true,
+
+	});
+});
