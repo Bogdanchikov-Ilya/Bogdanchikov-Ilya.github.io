83d787fb83d139226a6ce4a1dddaead99b9f5778
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-09-19 20:18:07 +0300

    Add files via upload

--- stream/script.js
+++ stream/script.js
@@ -8,6 +8,9 @@ let eat = new Image();
 
 let playerWidth = 128;
 let playerHeight = 128;
+let eatWidth = 47;
+let eatHei = 47;
+
 
 let a = 300;
 let b = 200;
@@ -49,62 +52,138 @@ function draw() {
 
     document.onkeydown = function move (){
         if(event.key == "ArrowRight"){
-            player.a+=20;
+            player.a+=25;
             if (player.a>540) {
                 player.a = 540;
             }
+            
 
         }
 
         if(event.key == "ArrowLeft") {
-            player.a-=20;
+            player.a-=25;
             if (player.a<0) {
                 player.a = 0;
             }
+            
         }
 
         if(event.key == "ArrowDown"){
-            player.b+=20;
+            player.b+=25;
             if (player.b>610) {
                 player.b = 610;
             }
         }
 
         if(event.key == "ArrowUp"){
-            player.b-=20;
+            player.b-=25;
             if (player.b<0) {
                 player.b = 0;
             }
         }
 
     }
-    if ( (bRandom > player.b && bRandom < player.b+playerHeight) && (aRandom > player.a && aRandom < player.a+playerWidth)){
+    if ( (bRandom + 40 > player.b && bRandom < player.b+playerHeight) && (aRandom + 30> player.a && aRandom + 30 < player.a+playerWidth)){
         aRandom = Math.round(Math.random()*620);
         bRandom = Math.round(Math.random()*670);
         score=score+1;
+        qwerty();
+        
         document.getElementById('score').innerHTML = score;
     }
 
+    function qwerty(){
+        //********************************1 lvl**************************************** */
     if(score >= 10) {
         document.getElementById('p1').disabled = false;
+        document.getElementById('e2').disabled = false;
+        document.getElementById('info').innerHTML = 'Вы достигли 2-го уровня';
+        
     }
     else if(score < 10) {
         document.getElementById('p1').disabled = true;
+        document.getElementById('e2').disabled = true;
     }
 
 
-    if(score >= 10) {
-        document.getElementById('p2').disabled = false;
+    
+//***********************************2 lvl************************************* */
+    if(score >= 20) {
+        document.getElementById('p3').disabled = false;
+        document.getElementById('e3').disabled = false;
+        document.getElementById('info').innerHTML = 'Вы достигли 3-го уровня';
+        
     }
-    else if(score < 10) {
-        document.getElementById('p2').disabled = true;
+    else if(score < 20) {
+        document.getElementById('p3').disabled = true;
+        document.getElementById('e3').disabled = true;
     }
 
     
+//***********************************3 lvl************************************* */
+    if(score >= 30) {
+        document.getElementById('p4').disabled = false;
+        document.getElementById('e4').disabled = false;
+        document.getElementById('info').innerHTML = 'Вы достигли 4-го уровня';
+    }
+    else if(score < 30) {
+        document.getElementById('p4').disabled = true;
+        document.getElementById('e4').disabled = true;
+    }
+
+   
+
+//***********************************4 lvl************************************* */
+    if(score >= 40) {
+        document.getElementById('p5').disabled = false;
+        document.getElementById('e5').disabled = false;
+        document.getElementById('info').innerHTML = 'Вы достигли 5-го уровня';
+    }
+    else if(score < 40) {
+        document.getElementById('p5').disabled = true;
+        document.getElementById('e5').disabled = true;
+    }
+
+   
+
+//***********************************5 lvl************************************* */
+if(score >= 50) {
+        document.getElementById('p6').disabled = false;
+        document.getElementById('e6').disabled = false;
+        document.getElementById('info').innerHTML = 'Вы достигли 6-го уровня';
+    }
+    else if(score < 50) {
+        document.getElementById('p6').disabled = true;
+        document.getElementById('e6').disabled = true;
+    }
+
+    
+    }
+
+    if (score == 0){
+        document.getElementById('p1').disabled = true;
+        document.getElementById('p3').disabled = true;
+        document.getElementById('p4').disabled = true;
+        document.getElementById('p5').disabled = true;
+        document.getElementById('p6').disabled = true;
+
+        document.getElementById('e2').disabled = true;
+        document.getElementById('e3').disabled = true;
+        document.getElementById('e4').disabled = true;
+        document.getElementById('e5').disabled = true;
+        document.getElementById('e6').disabled = true;
+
+    }
+    
+
+
 
 }
 
 
+function info () {
+    alert("Открыт новый скин")
+}
 
  draw();
 
@@ -115,6 +194,14 @@ function p1(num) {
 function p2(num) {
     eat.src = 'img/eat' + num + '.png';
 }
+
+function cheat(){
+    score=999;
+    score=score+1;
+    document.getElementById('score').innerHTML = score;
+    document.getElementById('info').innerHTML = "теперь вы читер";
+}
+
 
 function start() { p1(0); p2(0); draw() }
 start();
