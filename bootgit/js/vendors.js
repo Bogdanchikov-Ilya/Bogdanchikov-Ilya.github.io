d78956fafbad98cb390b570c42755dece0fb0d58
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-11-04 12:26:13 +0300

    Add files via upload

---
+++ bootgit/js/vendors.js
@@ -0,0 +1,12 @@
+function f1(){
+    if(document.querySelector('.menu-icon').classList.contains('active')){
+        document.querySelector('.menu-icon').classList.remove('active');
+    }else {
+        document.querySelector('.menu-icon').classList.add('active');
+
+    }
+
+    
+
+    
+};
