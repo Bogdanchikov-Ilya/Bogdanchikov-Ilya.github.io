627cedf3ba0353f5d49cd4eb9b871780e14fc440
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-11-05 22:21:03 +0300

    Add files via upload

---
+++ bootgit2/js/slider.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320:{
+            slidesPerView: 1,
+        },
+        534:{
+            slidesPerView: 2,
+        },
+        987:{
+            slidesPerView: 3,
+        },
+        1183:{
+            slidesPerView: 4,
+        }
+    }
+  });
