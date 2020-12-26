0eebce992e31cb88cb08bcb4d943fcf13fb57c0c
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-12-26 16:05:03 +0300

    Add files via upload

---
+++ vintic/script/menu.js
@@ -0,0 +1,14 @@
+function changeClass(){
+    document.querySelector(".check1").classList.toggle("menu__active");
+    document.querySelector(".check2").classList.toggle("menu__active");
+    document.querySelector(".check3").classList.toggle("menu__active");
+
+    document.querySelector(".container__in").classList.toggle("container__in-active");
+    document.querySelector(".hamburger-menu").classList.toggle("nav__check-active");
+
+    document.querySelector(".header__menu").classList.toggle("header__menu-active");
+
+    
+    
+    
+}
