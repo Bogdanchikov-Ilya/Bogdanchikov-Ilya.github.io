b03bc6bc6ed5b740aca3c1a265d920a25651d3ff
Bogdanchikov-Ilya <63511922+Bogdanchikov-Ilya@users.noreply.github.com>
2020-12-28 18:51:18 +0300

    Add files via upload

---
+++ menu example/script/menu.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    document.querySelector(".span__menu").classList.toggle("menu__active");
+
+    document.querySelector(".header__menu").classList.toggle("header__menu-active");
+
+    document.querySelector(".menu__list").classList.toggle("menu__list-active");
+
+}
